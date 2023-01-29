54d23f53eb5b5b12d9c63f0858b826afede9d6ef
RomanSit114 <83430725+RomanSit114@users.noreply.github.com>
2023-01-29 20:06:34 +0300

    Create config.py

---
+++ Module B/B10/telegram_bot/config.py
@@ -0,0 +1,10 @@
+exchanges = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
+TOKEN = "5896527034:AAFJq6WvgOyyl65NehIXDR9KzVU3nUDx3JY"
+
+headers= {
+  "apikey": "VTW7adfiyv7IpS3TeEVqKpHp1kdT48gG"
+}

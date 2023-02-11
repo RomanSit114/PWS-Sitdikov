3bd493e1c257bf3cb62c8a9d1183c29e5ce52122
RomanSit114 <83430725+RomanSit114@users.noreply.github.com>
2023-02-11 20:21:34 +0300

    Update task1.js

--- module_C/moduleC5_homework/task1.js
+++ module_C/moduleC5_homework/task1.js
@@ -1,1 +1,44 @@
+const parser = new DOMParser();
 
+const xmlString = `
+<list>
+  <student>
+    <name lang="en">
+      <first>Ivan</first>
+      <second>Ivanov</second>
+    </name>
+    <age>35</age>
+    <prof>teacher</prof>
+  </student>
+  <student>
+    <name lang="ru">
+      <first>Петр</first>
+      <second>Петров</second>
+    </name>
+    <age>58</age>
+    <prof>driver</prof>
+  </student>
+</list>
+`;
+
+const xmlDOM = parser.parseFromString(xmlString, "text/xml");
+const students = xmlDOM.querySelectorAll("student");
+
+const list = [];
+students.forEach((item) => {
+    const name = item.querySelector('name');
+    const firstNode = name.querySelector("first");
+    const secondNode = name.querySelector("second");
+    const ageNode = item.querySelector("age");
+    const profNode = item.querySelector("prof");
+    const nameAttr = name.getAttribute('lang');
+  
+    list.push({
+      name: `${firstNode.textContent} ${secondNode.textContent}`,
+      age: Number(ageNode.textContent),
+      prof: profNode.textContent,
+      lang: nameAttr
+    });
+});
+
+console.log({list});

5a8d1551469feb8d4272de8ed49d6630192ad0d8
RomanSit114 <83430725+RomanSit114@users.noreply.github.com>
2023-03-15 22:39:46 +0300

    Update task2.js

--- module_C/moduleC6_homework/task2/task2.js
+++ module_C/moduleC6_homework/task2/task2.js
@@ -1,1 +1,10 @@
+const btn = document.querySelector('.button-test');
 
+width = window.screen.width;
+height = window.screen.height;
+
+btn.addEventListener('click', () => {
+
+  window.alert(`Ширина: ${width}, длина: ${height}`);
+ 
+});
